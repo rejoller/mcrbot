58f1166685970815fe9185af6f4e6682f4a11ff1
Vlad_work <Vlad_work@gmai.com>
2024-09-05 12:24:54 +0700

    Исправления в файле с отпусками

--- handlers/vacation_file.py
+++ handlers/vacation_file.py
@@ -18,6 +18,6 @@ async def contacts_handler(message: types.Message, bot: Bot):
         file_info = await bot.get_file(document.file_id)
         await bot.download_file(file_info.file_path, destination)
         await message.answer_animation(caption=f'Файл с отпусками загружен.\nХорошего дня тебе, '
-                                       f'{user_name}', animation=random.choice(vacation_files))
+                                       f'{user_name}', animation=random.choice(vacation_files), show_caption_above_media=True)
     if "рафик" not in file_name:
         await message.answer('Имя файла не подходит для сохранения')
