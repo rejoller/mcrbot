7afcd3b0807e1bd753734b321db3b15bebac3a66
support <rublik211@gmail.com>
2024-09-06 08:23:11 +0000

    небольшие изменения файла

--- handlers/vacation_file.py
+++ handlers/vacation_file.py
@@ -18,13 +18,7 @@ async def contacts_handler(message: types.Message, bot: Bot):
         destination = os.path.join(os.getcwd(), directory, file_name)
         file_info = await bot.get_file(document.file_id)
         await bot.download_file(file_info.file_path, destination)
-        await message.answer_animation(caption=f'Файл с отпусками загружен.\nХорошего дня тебе, '
-<<<<<<< HEAD
-                                       f'{user_name}', animation=random.choice(vacation_files))
-
-
-=======
-                                       f'{user_name}', animation=random.choice(vacation_files), show_caption_above_media=True)
->>>>>>> 58f1166685970815fe9185af6f4e6682f4a11ff1
+        await message.answer_animation(caption=f'Файл с отпусками загружен.\nХорошего дня тебе, {user_name}',
+                                       animation=random.choice(vacation_files), show_caption_above_media=True)
     if "рафик" not in file_name:
         await message.answer('Имя файла не подходит для сохранения')
